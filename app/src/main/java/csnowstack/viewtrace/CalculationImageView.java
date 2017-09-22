c02445f5d466703efbbd247630b1625dacd86ae3
CSnowStack <csnowstack@gmail.com>
2017-09-23 01:46:39 +0800

    fit

---
+++ app/src/main/java/csnowstack/viewtrace/CalculationImageView.java
@@ -0,0 +1,22 @@
+package csnowstack.viewtrace;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ *
+ */
+
+public class CalculationImageView extends android.support.v7.widget.AppCompatImageView{
+    public CalculationImageView(Context context) {
+        super(context);
+    }
+
+    public CalculationImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CalculationImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/csnowstack/viewtrace/CalculationTextView.java
@@ -0,0 +1,26 @@
+package csnowstack.viewtrace;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+/**
+ *
+ */
+
+public class CalculationTextView extends AppCompatTextView{
+
+
+    public CalculationTextView(Context context) {
+        super(context);
+    }
+
+    public CalculationTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CalculationTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
